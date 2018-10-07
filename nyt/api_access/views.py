90aefe78528256472428e36cdcf47fd0df00efe2
martin-martin <breuss.martin@gmail.com>
2018-10-07 19:25:50 +0200

    remove non-form-based auth

--- nyt/api_access/views.py
+++ nyt/api_access/views.py
@@ -1,6 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-def index(request):
-    return HttpResponse("hei!")
+def frontpage(request):
+    if request.user.is_authenticated:
+        return redirect('/profile')
+    else:
+        if request.method == 'POST':
+            if 'signupform' in request.POST:
+                signupform = UserCreationForm(data=request.POST)
+                signinform = AuthenticationForm()
+
+                if signupform.is_valid():
+                    username = signupform.cleaned_data['username']
+                    password = signupform.cleaned_data['password1']
+                    signupform.save()
+                    user = authenticate(username=username, password=password)
+                    login(request, user)
+                    return redirect('/')
+
+            else:
+                signinform = AuthenticationForm(data=request.POST)
+                signupform = UserCreationForm()
+
+                if signinform.is_valid():
+                    login(request, signinform.get_user())
+                    return redirect('/')
+
+        else:
+            signupform = UserCreationForm()
+            signinform = AuthenticationForm()
+
+    return render(request, 'frontpage.html', {'signupform': signupform,
+                                              'signinform': signinform})
--- nyt/nyt/urls.py
+++ nyt/nyt/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # Add Django site authentication urls (for login, logout, pwd)
-    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('api_access.urls')),
     path('admin/', admin.site.urls),
 ]
